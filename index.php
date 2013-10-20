360022d9a077114aa4d9f0e57f8e91f22a8cb538
boboman13 <brendan@brendan-ubuntu.(none)>
2013-10-19 18:59:28 -0700

    Make it work a little bit.

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
 
-/* Blogza. */
+/* Welcome to Blogza! Blogza is an open source blogging software, designed to
+ *  1) Allow customization down to the hard HTML
+ *  2) Keep blogging simple and easy
+ *  3) Keep setup fast and simple
+ *
+ * We hope you enjoy the software! - boboman13
+ */
+
+error_reporting(E_ALL);
+
+/* Load 'mainframe.php'
+ *
+ * Includes basic mainframe code
+ */
+
+require('system/mainframe.php');
+
+/* Creates Blogza object.
+ *
+ * Used for creating the blog.
+ */
+$blogza = new Blogza();
+
+/* Start $blogza.
+ *
+ * No more functions are needed in this file.
+ */
+$blogza->start();
+
