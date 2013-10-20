2696c97da5807666a5bfe39ad589b9f43af7aede
boboman13 <rich@safe-node.com>
2013-10-19 23:12:07 -0700

    Change the settings a bit by adding a description.

--- system/settings.php
+++ system/settings.php
@@ -1,4 +1,7 @@
 <?php
 
+/* Settings file for the Blogza blog. Fill these out according to the wiki. 
+ * https://github.com/boboman13/Blogza/wiki/Setting-up-the-configuration-file
+ */
 define("BLOG_NAME", "My Blog");
 define("BLOG_DESC", "This is a blog powered by the Blogza blog framework.");
