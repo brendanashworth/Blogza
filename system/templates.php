84d52957ac7c991f73a4557db61b50c30ebe58d4
boboman13 <brendan@brendan-ubuntu.(none)>
2013-10-19 18:58:39 -0700

    Make it work

--- system/templates.php
+++ system/templates.php
@@ -4,21 +4,56 @@ class TemplateManager {
 
 	public $name;
 
-	function __construct($template = "default") {
+	public $page;
+
+	/**
+	* Creates the TemplateManager instance.
+	*
+	* @param template The name of the template file to use.
+	*
+	**/
+	public function __construct($template) {
 		$this->name = $template;
 
-		loadTemplate();
+		$this->loadTemplate();
 	}
 
-	function loadTemplate() {
+	/**
+	* Loads the template from the files.
+	*
+	* @access private
+	**/
+	private function loadTemplate() {
 		// First the header
-		include __DIR__.'/../templates/'.$this->name.'/header.html';
+		$this->page = file_get_contents(__DIR__.'/../templates/'.$this->name.'/header.html');
 
 		// Then the sidebar
 
 		// Then the body
 
 		// Lastly, the footer
+
+		$this->processPage();
+	}
+
+	/**
+	* Processes the page. This includes changing all variables to their specified values.
+	*
+	* @access private
+	**/
+	private function processPage() {
+		$this->page = str_replace("{blog-name}", BLOG_NAME, $this->page);
+
+		$this->displayPage();
+	}
+
+	/**
+	* Displays the page.
+	*
+	* @access private
+	**/
+	private function displayPage() {
+		echo $this->page;
 	}
 	
 }
