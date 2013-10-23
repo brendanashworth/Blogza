7e646909d725e3203d933def19a47e2a0a3854c2
boboman13 <rich@safe-node.com>
2013-10-23 00:02:12 -0700

    MySQL works now, integration finishing up.

--- system/assets/sql/initialize_db.sql
+++ system/assets/sql/initialize_db.sql
@@ -3,5 +3,6 @@ CREATE TABLE `posts` {
 	post_author varchar(20) NOT NULL,
 	post_title varchar(100) NOT NULL,
 	post_content varchar(10000) NOT NULL,
-	post_date varchar(10) NOT NULL
+	post_date varchar(10) NOT NULL,
+	PRIMARY KEY (id)
 }
--- system/dbloader.php
+++ system/dbloader.php
@@ -7,6 +7,7 @@ class DatabaseManager {
 	/**
 	* Creates the DatabaseManager instance.
 	*
+	* @access	public
 	* @return	DatabaseManager
 	**/
 	public function __construct() {
@@ -14,7 +15,7 @@ class DatabaseManager {
 
 		// Check for error.
 		if(mysqli_connect_errno($this->mysqli)) {
-			// die("Error: Could not connect to MySQL database.");
+			die("Error: Could not connect to MySQL database.");
 		}
 
 	}
@@ -26,35 +27,42 @@ class DatabaseManager {
 	* @return	mixed
 	**/
 	public function getPosts() {
-		$result = $this->queryDB("SELECT * FROM posts"); // This query does not require sanitization because it is never changed.
+		$query = "SELECT * FROM `posts`";
 
-		return array(
-			"1" => array(
-				"id" => 1,
-				"author" => "boboman13",
-				"title" => "This is a post title.",
-				"content" => "Hello, and welcome to the blog!",
-				"link" => "/post/1",
-				),
-			"2" => array(
-				"id" => 2,
-				"author" => "Author2",
-				"title" => "Post #2",
-				"content" => "content",
-				"link" => "/post/2",
-				),
-			"3" => array(
-				"id" => 3,
-				"author" => "Author3",
-				"title" => "Title Post #3",
-				"content" => "content for #3",
-				"link" => "/post/3",
-				),
-			);
+		$result = $this->queryDB($query);
+
+		/* Now we format it into our standard post format. */
+		$posts = array();
+		while($row = $result->fetch_assoc()) {
+			$id = $row['id'];
+
+			$posts[$id] = array(
+				"id" => $id,
+				"author" => $row['author'],
+				"title" => $row['title'],
+				"content" => $row['content'],
+				"date" => $row['date'],
+				"link" => "",
+				);
+		}
+
+		return $posts;
 	}
 
 	/**
-	* Querys the database. This method includes basic sanitization (SANITIZE BEFORE) and then returns the result.
+	* Sees whether a username is valid or not.
+	*
+	* @access	public
+	* @return	boolean
+	**/
+	public function isValidUser($username) {
+		$username = mysqli_real_escape_string($this->mysqli, $username);
+		$query = "SELECT * FROM `users` WHERE ";
+
+	}
+
+	/**
+	* Querys the database.
 	*
 	* @access	private
 	* @return	mixed
@@ -65,14 +73,13 @@ class DatabaseManager {
 			throw new Exception("The query cannot be null!");
 		}
 
-		// Sanitize.
-		$query = mysqli_real_escape_string($this->mysqli, $query); // Do some MySQLi generic sanitization
-		$query = htmlspecialchars($query); // Neutralize HTML
-
+		// Query!
 		$result = mysqli_query($this->mysqli, $query);
 
-		// Sanitize again!
-		$result = htmlspecialchars($result);
+		// Did we encounter an error?
+		if( !$result ) {
+			die("MySQL Query Error.");
+		}
 
 		return $result;
 	}
