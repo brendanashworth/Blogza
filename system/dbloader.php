f1bda3cecdbdf1de3e4290a5cc4781bf43488907
boboman13 <rich@safe-node.com>
2013-10-25 23:49:45 -0700

    Works again.

--- system/dbloader.php
+++ system/dbloader.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+* Blogza, the open source, simple to use blogging software.
+*
+* @author	boboman13 <me@boboman13.net>
+* @version	1.0
+**/
 class DatabaseManager {
 
 	private $mysqli;
@@ -21,13 +27,48 @@ class DatabaseManager {
 	}
 
 	/**
+	* Creates and inserts the post into the database.
+	*
+	* @access public
+	* @param	string	$author		The author of the post.
+	* @param	string	$title 		The title of the post.
+	* @param	string 	$content 	The content of the post.
+	* @return boolean
+	**/
+	public function createPost($author = null, $title = null, $content = null) {
+		// If any were null, throw an exception.
+		if($author == null || $title == null || $content == null) {
+			throw new Exception("The author, title, or content cannot be null!");
+		}
+
+		// Sanitize these inputs.
+		$author  = mysqli_real_escape_string($author);
+		$title   = mysqli_real_escape_string($title);
+		$content = mysqli_real_escape_string($content);
+
+		// Here is the query and execution.
+		$query = "INSERT INTO `posts` (author, title, content) VALUES ('$author', '$title', '$content')";
+
+		$result = $this->queryDB($query);
+
+		// Give a result.
+		if($result == true || $result == false) {
+			return $result;
+		} else {
+			// Uh... why didn't it return true or false? Was it just bypassed?
+			return false;
+		}
+
+	}
+
+	/**
 	* Gets all the posts in array format.
 	*
 	* @access	public
 	* @return	mixed
 	**/
 	public function getPosts() {
-		$query = "SELECT * FROM `posts`";
+		$query = "SELECT * FROM `posts`"; // Direct query into DB, no variables.
 
 		$result = $this->queryDB($query);
 
@@ -50,11 +91,75 @@ class DatabaseManager {
 	}
 
 	/**
+	* Creates and inserts a new user into the database.
+	*
+	* @access	public
+	* @param	string	$username	The user's display name. This is used for all blog posts.
+	* @param	string	$password	The user's password. This will be hashed in this function.
+	* @return	mixed
+	**/
+	public function createUser($username = null, $password = null) {
+		// If any given variables were null, throw an exception.
+		if($username == null || $password == null) {
+			throw new Exception("The username or password cannot be null!");
+		}
+
+		// Sanitize.
+		$username = mysqli_real_escape_string($username);
+		$password = mysqli_real_escape_string($password);
+
+		// Hash the password. (Iterate 16 times for security!)
+		for($i = 0; $i < 16; $i++) {
+			$password = hash('sha256', $password);
+		}
+
+		$query = "INSERT INTO `users` (user_name, user_password, user_posts) VALUES ('$username', '$password', '0')";
+
+		$result = $this->queryDB($query);
+
+		// Give a result.
+		if($result == true || $result == false) {
+			return $result;
+		} else {
+			// Uh... why didn't it return true or false? Was it just bypassed?
+			return false;
+		}
+	}
+
+	/**
+	* Gets the user from the database.
+	*
+	* @access	public
+	* @param	string	$username	The user's name.
+	* @return	mixed	Returns the User object if found; null if not found.
+	**/
+	public function getUser($username) {
+		$query = "SELECT * FROM `users`";
+
+		$result = $this->queryDB($query);
+
+		$found = false;
+		// Lets find our user.
+		while($row = $result->fetch_assoc()) {
+			if($row['user_name'] == $username) $found = $row;
+		}
+
+		if($found == false) {
+			// Return null
+			return null;
+		} else {
+			// Make the user.
+			$user = new User($found['user_name'], $found['user_password'], $found['user_posts']);
+			return $user;
+		}
+	}
+
+	/**
 	* Querys the database.
 	*
 	* @access	private
+	* @param	string 	$query 		The string of SQL to query the database with.
 	* @return	mixed
-	* @param	string	$query The string of SQL to query the database with.
 	**/
 	private function queryDB($query = null) {
 		if($query == null) {
--- system/mainframe.php
+++ system/mainframe.php
@@ -1,13 +1,11 @@
 <?php
 
-/* Welcome to Blogza! Blogza is an open source blogging software, designed to
- *  1) Allow customization down to the hard HTML
- *  2) Keep blogging simple and easy
- *  3) Keep setup fast and simple
- *
- * We hope you enjoy the software! - boboman13
- */
-
+/**
+* Blogza, the open source, simple to use blogging software.
+*
+* @author	boboman13 <me@boboman13.net>
+* @version	1.0
+**/
 class Blogza {
 
 	public static $name = "Blogza";
@@ -16,6 +14,17 @@ class Blogza {
 	private $templatemanager;
 	private $router;
 	private $routes;
+
+	/**
+	* Creates the Blogza instance.
+	*
+	* @access 	public
+	* @return 	Blogza 	The Blogza instance.
+	**/
+	public function __construct() {
+		// Creates the Blogza instance. This requires all the .class.php files!
+		//require __DIR__.'/includes/user.class.php';
+	}
 
 	/**
 	* Starts Blogza.
@@ -47,9 +56,10 @@ class Blogza {
 		$this->templatemanager = new TemplateManager($this);
 
 		$this->routes->prepareRouter();
-		
+
 		/* Display the generated info. */
 		$router->go();
+
 	}
 
 	/**
