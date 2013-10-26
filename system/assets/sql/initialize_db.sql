d6038d41285a403f665da868473db5d25600ad7d
boboman13 <rich@safe-node.com>
2013-10-25 23:49:45 -0700

    Works again.

--- system/assets/sql/initialize_db.sql
+++ system/assets/sql/initialize_db.sql
@@ -1,8 +1,16 @@
 CREATE TABLE `posts` {
-	id int(10) NOT NULL AUTO_INCREMENT,
-	post_author varchar(20) NOT NULL,
-	post_title varchar(100) NOT NULL,
-	post_content varchar(10000) NOT NULL,
-	post_date varchar(10) NOT NULL,
+	`id` int(10) NOT NULL AUTO_INCREMENT,
+	`post_author` varchar(20) NOT NULL,
+	`post_title` varchar(100) NOT NULL,
+	`post_content` varchar(10000) NOT NULL,
+	`post_date` varchar(10) NOT NULL,
+	PRIMARY KEY (id)
+}
+
+CREATE TABLE `users` {
+	`id` int(10) AUTO_INCREMENT NOT NULL,
+	`user_name` varchar(16) NOT NULL,
+	`user_password` varchar(256) NOT NULL,
+	`user_posts` int(10) NOT NULL,
 	PRIMARY KEY (id)
 }
