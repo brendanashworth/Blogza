bcdb03d587d915032719658ac4a6e9eb32649945
boboman13 <rich@safe-node.com>
2013-11-03 22:58:20 -0800

    Finishing up MVC.

--- index.php
+++ index.php
@@ -1,28 +1,32 @@
 <?php
 
-/* Welcome to Blogza! Blogza is an open source blogging software, designed to
- *  1) Allow customization down to the hard HTML
- *  2) Keep blogging simple and easy
- *  3) Keep setup fast and simple
- *
- * We hope you enjoy the software! - boboman13
- */
-
-/* Load 'mainframe.php'
- *
- * Includes basic mainframe code
+/* ----------------------------------------
+ | Requires the Blogza mainframe PHP file
+ | ----------------------------------------
+ | This loads the Blogza mainframe code
+ | via `require`. This allows for the
+ | creation of our Blogza object, later
+ | utilized in the code.
  */
 
 require('system/Blogza.php');
 
-/* Creates Blogza object.
- *
- * Used for creating the blog.
+/* ----------------------------------------
+ | Creates the Blogza object
+ | ----------------------------------------
+ | This function creates the Blogza object
+ | and assigns it to $blogza. This is our
+ | one and only Blogza instance in the
+ | software, and runs the entire software.
  */
 $blogza = new Blogza();
 
-/* Start $blogza.
- *
- * No more functions are needed in this file.
+/* ----------------------------------------
+ | Starts the Blogza blogging software
+ | ----------------------------------------
+ | This function starts the Blogza blog.
+ | Once started, it runs by itself from
+ | the Blogza framework files. It then
+ | then displays all output itself.
  */
 $blogza->start();
