40f4eeb8ed998ccd97111a1c141fdd6e145b98d9
boboman13 <rich@safe-node.com>
2014-02-02 15:06:04 -0500

    Adds gzip for faster downloads.

--- system/controllers/MinificationController.php
+++ system/controllers/MinificationController.php
@@ -19,6 +19,17 @@ class MinificationController extends Controller {
 		);
 
 	/**
+	* Set the expires header.
+	*
+	* @access 	private
+	* @param 	int 	$time 	The amount of seconds the object should be locally cached for. This defaults to 7200, or 2 hours.
+	* @return 	void
+	**/
+	private function setExpires($time = 7200) {
+		header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + $time));
+	}
+
+	/**
 	* Gets a css website asset.
 	*
 	* @access 	public
@@ -30,11 +41,14 @@ class MinificationController extends Controller {
 
 		if(file_exists($route . $filename)) {
 			header("Content-type: text/css");
+			$this->setExpires(3600);
 
 			$contents = file_get_contents($route . $filename);
 			$minify = new Minify();
 
 			$file = $minify->minifyCSS($contents);
+
+			ob_start("ob_gzhandler");
 			echo $file;
 			ob_end_flush();
 		} else {
@@ -55,11 +69,14 @@ class MinificationController extends Controller {
 
 		if(file_exists($route . $filename)) {
 			header("Content-type: application/javascript");
+			$this->setExpires(3600);
 
 			$contents = file_get_contents($route . $filename);
 			$minify = new Minify();
 
 			$file = $minify->minifyJS($contents);
+
+			ob_start("ob_gzhandler");
 			echo $file;
 			ob_end_flush();
 		} else {
@@ -80,9 +97,11 @@ class MinificationController extends Controller {
 
 		if(file_exists($route . $filename)) {
 			header("Content-type: application/octet-stream");
+			$this->setExpires();
 
 			$contents = file_get_contents($route . $filename);
 
+			ob_start("ob_gzhandler");
 			echo $contents;
 			ob_end_flush();
 		} else {
@@ -118,9 +137,11 @@ class MinificationController extends Controller {
 
 			// Deliver it.
 			header("Content-type: " . $mimetype);
+			$this->setExpires();
 
 			$contents = file_get_contents($route . $filename);
 
+			ob_start("ob_gzhandler");
 			echo $contents;
 			ob_end_flush();
 		} else {
